23a9bd9a8c286306e9c8a3ed094ea511caca8551
GabrielCMWerner <gabrielcmw1094@gmail.com>
2023-04-10 14:39:09 -0300

    Add project files.

---
+++ ContaCorrente/Movimentacao.cs
@@ -0,0 +1,14 @@
+﻿namespace ContaCorrente
+{
+    public class Movimentacao
+    {
+        public double Valor;
+        public string Tipo;
+            
+        public Movimentacao(double valor, string tipo)
+        {
+            Valor = valor;
+            Tipo = tipo;
+        }
+    }
+}
